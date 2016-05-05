6668cc81f0d60233909daa8e2b295dbb80bdde54
Ablex.Gu <P4_gumingxuan@163.com>
2016-05-06 06:54:29 +0800

    添加类的学习文件

---
+++ 类的学习/类的学习/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  类的学习
+//
+//  Created by 顾明轩 on 16/5/6.
+//  Copyright © 2016年 顾明轩. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
