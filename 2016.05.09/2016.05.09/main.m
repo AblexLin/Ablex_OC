d3cd72e000a47ad134a9aea81ad314663b947fce
Ablex.Gu <P4_gumingxuan@163.com>
2016-05-09 17:58:30 +0800

    添加字符串的一些操作

---
+++ 2016.05.09/2016.05.09/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  2016.05.09
+//
+//  Created by 顾明轩 on 16/5/9.
+//  Copyright © 2016年 顾明轩. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
