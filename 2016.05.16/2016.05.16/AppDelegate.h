8c9d9395fe26795abe5dc77615d327a2add474fc
Ablex.Gu <P4_gumingxuan@163.com>
2016-05-16 22:58:57 +0800

    2016.05.16 1.UITextField 2.UITextView 3.UISegmentedControl 4.UISwitch 5.UISlider

--- 2016.05.16/2016.05.16/AppDelegate.h
+++ 2016.05.16/2016.05.16/AppDelegate.h
@@ -15,22 +15,24 @@
 //声明label
 @property (strong, nonatomic) UILabel *label_user;
 @property (strong, nonatomic) UILabel *label_code;
+@property (strong, nonatomic) UILabel *label;
 
 //声明button
 @property (strong, nonatomic) UIButton *button_showinfo;
 @property (strong, nonatomic) UIButton *button_clearinfo;
+@property (strong, nonatomic) UIButton *button;
 
 //声明textfield
 @property (strong, nonatomic) UITextField *textfield_user;
 @property (strong, nonatomic) UITextField *textfield_code;
+@property (strong, nonatomic) UITextField *textfield;
 
 //声明textview
 @property (strong, nonatomic) UITextView *textview_show;
+@property (strong, nonatomic) UITextView *textview;
 
-
-//声明textfield求和用
+//声明textfield,button求和用
 @property (strong, nonatomic) UITextField *textfield_sum;
-//声明button求和用
 @property (strong, nonatomic) UIButton *button_sum;
 
 @end
--- 2016.05.16/2016.05.16/AppDelegate.m
+++ 2016.05.16/2016.05.16/AppDelegate.m
@@ -40,87 +40,85 @@
     self.window.rootViewController=root;
     self.window.backgroundColor=[UIColor whiteColor];
     [self.window makeKeyAndVisible];
-    //视频学习的部分
-    /*
-    //UITextField文本输入框的学习
-    //文本框只接收单行文本
-    _textField=[[UITextField alloc]initWithFrame:CGRectMake(10, 40, 200, 50)];
-    _textField.backgroundColor=[UIColor lightGrayColor];//设定背景颜色
-    _textField.textColor=[UIColor blueColor];//设定文本颜色
-//    textField.text=@"我擦!你看到我了";//设定文字颜色
-
-    _textField.textAlignment=NSTextAlignmentCenter;//居中对齐
-    _textField.keyboardAppearance=UIKeyboardAppearanceDefault;
-//    _textField.keyboardType=UIKeyboardTypeNumberPad;//设定为数字键盘
-    _textField.returnKeyType=UIReturnKeyGo;//设定return显示的内容是什么
-    _textField.placeholder=@"请输入用户名";//设置显示的阴影，只有在文本框内没有内容的时候才显示，对齐方式取决于文本框的对齐方式
-    _textField.secureTextEntry=YES;//安全加密输入的数字会变成点
     
-    //多行文本框
-    UITextView *tv=[[UITextView alloc]initWithFrame:CGRectMake(10, 100, 200, 50)];
-    tv.backgroundColor=[UIColor lightGrayColor];
-    tv.secureTextEntry=YES;//无效，也没有placeholder的属性
-    tv.text=@"nima\nwca";//可以这样换行
-    [self.window addSubview:tv];
+    //文本输入框(UITextField)
+    //文本框只接收单行文本
+    self.textfield=[[UITextField alloc]initWithFrame:CGRectMake(10, 40, 200, 50)];      //初始化个对象
+    self.textfield.backgroundColor=[UIColor lightGrayColor];                            //设定背景颜色
+    self.textfield.textColor=[UIColor blueColor];                                       //设定文本颜色
+    self.textfield.text=@"我擦!你看到我le";                                               //设定文字颜色
+    self.textfield.textAlignment=NSTextAlignmentCenter;                                 //设定居中对齐
+    self.textfield.keyboardAppearance=UIKeyboardAppearanceDefault;                      //设定显示模式
+    self.textfield.keyboardType=UIKeyboardTypeNumberPad;                                //设定数字键盘
+    self.textfield.returnKeyType=UIReturnKeyGo;                                         //设定返回模式
+    self.textfield.placeholder=@"请输入用户名";                                           //设定显示的阴影，只有在文本框内没有内容的时候才显示，对齐方式取决于文本框的对齐方式
+    self.textfield.secureTextEntry=YES;                                                 //设定安全加密输入的数字会变成点
+    
+    //多行文本框(UITextView)
+    self.textview=[[UITextView alloc]initWithFrame:CGRectMake(10, 100, 200, 50)];       //初始化个对象
+    self.textview.backgroundColor=[UIColor lightGrayColor];                             //设定背景颜色
+    self.textview.secureTextEntry=YES;                                                  //设定无效，也没有placeholder的属性
+    self.textview.text=@"nima\nwca";                                                    //可以这样换行
     
     //实现获取输入的内容，添加一个按钮
-    UIButton *button=[[UIButton alloc]initWithFrame:CGRectMake(220, 40, 50, 50)];
-    button.backgroundColor=[UIColor greenColor];
-    [button setTitle:@"取值" forState:UIControlStateNormal];
-    [button addTarget:self action:@selector(pushTap:) forControlEvents:UIControlEventTouchUpInside];
-    [self.window addSubview:button];
+    self.button=[[UIButton alloc]initWithFrame:CGRectMake(220, 40, 50, 50)];            //初始化个按钮
+    self.button.backgroundColor=[UIColor greenColor];                                   //设定背景颜色
+    [self.button setTitle:@"取值le" forState:UIControlStateNormal];                      //设定按钮显示
+    [self.button addTarget:self action:@selector(buttonTap:) forControlEvents:UIControlEventTouchUpInside];//添加按钮点击事件
+
     //放到窗口上
-    [self.window addSubview:_textField];
-    */
-    //diy part
+    //[self.window addSubview:self.textfield];
+    //[self.window addSubview:self.textview];
+    //[self.window addSubview:self.button];
     
+    //diy part
     //创建用户名的label
-    label_user=[[UILabel alloc]initWithFrame:CGRectMake(10, 30, 60, 30)];
-    label_user.text=@"用户名:";
-    label_user.textAlignment=NSTextAlignmentCenter;
-    label_user.font=[UIFont systemFontOfSize:13];
-    label_user.textColor=[UIColor lightGrayColor];
+    label_user=[[UILabel alloc]initWithFrame:CGRectMake(10, 30, 60, 30)];                //初始化个对象
+    label_user.text=@"用户名:";                                                           //设定显示文本
+    label_user.textAlignment=NSTextAlignmentCenter;                                      //设定对齐方式
+    label_user.font=[UIFont systemFontOfSize:13];                                        //设定字体大小
+    label_user.textColor=[UIColor lightGrayColor];                                       //设定字体颜色
     //创建密码的label
-    label_code=[[UILabel alloc]initWithFrame:CGRectMake(10, 70, 60, 30)];
+    label_code=[[UILabel alloc]initWithFrame:CGRectMake(10, 70, 60, 30)];                //同上
     label_code.text=@"密码:";
     label_code.textAlignment=NSTextAlignmentCenter;
     label_code.font=[UIFont systemFontOfSize:13];
     label_code.textColor=[UIColor lightGrayColor];
     
     //创建输入用户名数据多输入框textfield
-    textfield_user=[[UITextField alloc]initWithFrame:CGRectMake(80, 30, 180, 30)];
-    textfield_user.backgroundColor=[UIColor lightGrayColor];
-    textfield_user.textAlignment=NSTextAlignmentCenter;
-    textfield_user.font=[UIFont systemFontOfSize:13];
-    textfield_user.placeholder=@"请输入您的用户名...";
+    textfield_user=[[UITextField alloc]initWithFrame:CGRectMake(80, 30, 180, 30)];       //初始化个对象
+    textfield_user.backgroundColor=[UIColor lightGrayColor];                             //设定背景颜色
+    textfield_user.textAlignment=NSTextAlignmentCenter;                                  //设定对齐方式
+    textfield_user.font=[UIFont systemFontOfSize:13];                                    //设定字体大小
+    textfield_user.placeholder=@"请输入您的用户名...";                                      //设定阴影效果
     //创建输入密码数据多输入框textfield
-    textfield_code=[[UITextField alloc]initWithFrame:CGRectMake(80, 70, 180, 30)];
+    textfield_code=[[UITextField alloc]initWithFrame:CGRectMake(80, 70, 180, 30)];       //同上
     textfield_code.backgroundColor=[UIColor lightGrayColor];
     textfield_code.textAlignment=NSTextAlignmentCenter;
     textfield_code.font=[UIFont systemFontOfSize:13];
     textfield_code.placeholder=@"请输入您的密码...";
-    textfield_code.secureTextEntry=YES;
+    textfield_code.secureTextEntry=YES;                                                  //设定隐藏密码
     
     //创建读取label和textfield数据的按钮
-    button_showinfo=[[UIButton alloc]init];
-    button_showinfo.frame=CGRectMake(90, 110, 50, 30);
-    button_showinfo.backgroundColor=[UIColor lightGrayColor];
-    [button_showinfo setTitle:@"显示" forState:UIControlStateNormal];
-    button_showinfo.titleLabel.font=[UIFont systemFontOfSize:13];
-    [button_showinfo addTarget:self action:@selector(showinfo:) forControlEvents:UIControlEventTouchUpInside];
+    button_showinfo=[[UIButton alloc]init];                                              //初始化个按钮
+    button_showinfo.frame=CGRectMake(90, 110, 50, 30);                                   //设置按钮大小
+    button_showinfo.backgroundColor=[UIColor lightGrayColor];                            //设置按钮背景
+    [button_showinfo setTitle:@"显示" forState:UIControlStateNormal];                     //设置按钮文字
+    button_showinfo.titleLabel.font=[UIFont systemFontOfSize:13];                        //设置文字大小
+    [button_showinfo addTarget:self action:@selector(showinfoTap:) forControlEvents:UIControlEventTouchUpInside];//设置按钮行为
     //创建清除label和textfield数据的按钮
-    button_clearinfo=[[UIButton alloc]init];
+    button_clearinfo=[[UIButton alloc]init];                                             //同上
     button_clearinfo.frame=CGRectMake(180, 110, 50, 30);
     button_clearinfo.backgroundColor=[UIColor lightGrayColor];
     [button_clearinfo setTitle:@"清除" forState:UIControlStateNormal];
     button_clearinfo.titleLabel.font=[UIFont systemFontOfSize:13];
-    [button_clearinfo addTarget:self action:@selector(clearinfo:) forControlEvents:UIControlEventTouchUpInside];
+    [button_clearinfo addTarget:self action:@selector(clearinfoTap:) forControlEvents:UIControlEventTouchUpInside];
     
     //新建一个多行输入框，用来显示信息
-    textview_show=[[UITextView alloc]initWithFrame:CGRectMake(60, 150, 220, 45)];
-    textview_show.backgroundColor=[UIColor lightGrayColor];
-    textview_show.textAlignment=NSTextAlignmentCenter;
-    textview_show.font=[UIFont systemFontOfSize:13];
+    textview_show=[[UITextView alloc]initWithFrame:CGRectMake(60, 150, 220, 45)];        //初始化个对象
+    textview_show.backgroundColor=[UIColor lightGrayColor];                              //设置背景颜色
+    textview_show.textAlignment=NSTextAlignmentCenter;                                   //设置对齐方式
+    textview_show.font=[UIFont systemFontOfSize:13];                                     //设置字体大小
     
     
     //求和用的textfield
@@ -135,37 +133,94 @@
     button_sum.backgroundColor=[UIColor lightGrayColor];
     button_sum.titleLabel.textColor=[UIColor lightGrayColor];
     button_sum.titleLabel.font=[UIFont systemFontOfSize:13];
-    [button_sum addTarget:self action:@selector(sum:) forControlEvents:UIControlEventTouchUpInside];
+    [button_sum addTarget:self action:@selector(sumTap:) forControlEvents:UIControlEventTouchUpInside];
     
     //将控件添加到桌面上去
-    [self.window addSubview:label_user];
-    [self.window addSubview:label_code];
-    [self.window addSubview:textfield_user];
-    [self.window addSubview:textfield_code];
-    [self.window addSubview:button_showinfo];
-    [self.window addSubview:button_clearinfo];
-    [self.window addSubview:textview_show];
+    //[self.window addSubview:label_user];
+    //[self.window addSubview:label_code];
+    //[self.window addSubview:textfield_user];
+    //[self.window addSubview:textfield_code];
+    //[self.window addSubview:button_showinfo];
+    //[self.window addSubview:button_clearinfo];
+    //[self.window addSubview:textview_show];
+    //将求和的控件添加到桌面
+    //[self.window addSubview:textfield_sum];
+    //[self.window addSubview:button_sum];
     
-    [self.window addSubview:textfield_sum];
-    [self.window addSubview:button_sum];
+    //switch控件
+    UISwitch *switch1=[[UISwitch alloc]initWithFrame:CGRectMake(10, 30, 50, 50)];
+    [switch1 addTarget:self action:@selector(switchTap:) forControlEvents:UIControlEventValueChanged];
+    switch1.tag=15;
+
+    UISwitch *switch2=[[UISwitch alloc]initWithFrame:CGRectMake(80, 30, 50, 50)];
+    [switch2 addTarget:self action:@selector(switchTap:) forControlEvents:UIControlEventValueChanged];
+    switch2.tag=30;
+    //上面两个开关都调用了swch这个行为，那么，在行为里面如何区别到底是哪一个按钮呢？
+    //一般情况下我们把这个两个开关在h文件里面声明为类的数据
+    //今天使用新的办法，tag～～～
+    //[self.window addSubview:switch1];
+    //[self.window addSubview:switch2];
     
+    if ([switch1 isOn]) {
+        NSLog(@"如果窗口打开了");
+        switch1.backgroundColor=[UIColor grayColor];
+    }
+    else{
+        NSLog(@"如果窗口关闭了");
+        switch1.backgroundColor=[UIColor blackColor];
+    }
+    
+    //分段控件UISegmentedControl
+    NSArray *array=[NSArray arrayWithObjects:@"轩爷",@"首长",@"老人头", nil];                      //定制一个数组用来作为分段的显示名称
+    UISegmentedControl *seg=[[UISegmentedControl alloc]initWithItems:array];                    //初始化一个分段控件
+    seg.frame=CGRectMake(10, 30, 200, 30);                                                      //设定控件大小
+    seg.selectedSegmentIndex=0;                                                                 //默认选择第0个
+    [seg addTarget:self action:@selector(segTap:) forControlEvents:UIControlEventValueChanged]; //设置切换行为
+    
+    label_user=[[UILabel alloc]initWithFrame:CGRectMake(220, 30, 70, 30)];
+    label_user.textColor=[UIColor blueColor];
+    label_user.text=[seg titleForSegmentAtIndex:0];
+    
+    //添加到窗口
+    //[self.window addSubview:label_user];
+    //[self.window addSubview:seg];
+    
+    //slider
+    UISlider *slider=[[UISlider alloc]initWithFrame:CGRectMake(10, 30, 200, 10)];
+    slider.minimumValue=0;                                                                              //设置最小值
+    slider.maximumValue=10;                                                                             //设置最大值
+    slider.value=5;                                                                                     //设置当前值
+    [slider addTarget:self action:@selector(sliderTap:) forControlEvents:UIControlEventValueChanged];   //设置行为
+    textfield_user=[[UITextField alloc]initWithFrame:CGRectMake(220, 30, 50, 20)];
+    
+    textfield_user.enabled=NO;                                                                          //文本框不能输入了
+    
+    //[self.window addSubview:textfield_user];
+    //[self.window addSubview:slider];
     return YES;
 }
-//实现点击显示后的行为
--(void)showinfo:(UIButton *)infoSender
+//实现按钮点击的行为
+-(void)buttonTap:(UIButton *)sender
 {
-    textview_show.text=[NSString stringWithFormat:@"%@%@\n%@%@",label_user.text,textfield_user.text,label_code.text,textfield_code.text];
+    NSLog(@"%@",self.textfield.text);//可以读取到
+    UIAlertView *alert=[[UIAlertView alloc]initWithTitle:@"报警" message:@"读到了" delegate:self cancelButtonTitle:@"取消吧" otherButtonTitles:@"heihei",@"haha", nil];
+    [alert show];
 }
 
 //实现点击显示后的行为
--(void)clearinfo:(UIButton *)infoSender
+-(void)showinfoTap:(UIButton *)infoSender
+{
+    textview_show.text=[NSString stringWithFormat:@"%@%@\n%@%@",label_user.text,textfield_user.text,label_code.text,textfield_code.text];
+}
+//实现点击清除后的行为
+-(void)clearinfoTap:(UIButton *)infoSender
 {
     textfield_user.text=@"";
     textfield_code.text=@"";
     textview_show.text=@"";
 }
-
--(void)sum:(UIButton *)sumSender
+//实现点击求和后的行为
+-(void)sumTap:(UIButton *)sumSender
 {
     UIAlertView *alert=[[UIAlertView alloc]initWithTitle:@"请注意" message:@"输入的为空!" delegate:nil cancelButtonTitle:@"我知道了" otherButtonTitles:nil];
     if ([textfield_sum.text isEqualToString:@""]) {
@@ -176,40 +231,60 @@
         sum+=i;
     }
     textfield_sum.text=[NSString stringWithFormat:@"%d",sum];
-    
-
-    
 }
 
-//视频学习的部分
--(void)pushTap:(UIButton *)sender
+//实现switch切换的行为
+-(void)switchTap:(UISwitch *)sender
 {
-//    NSLog(@"%@",_textField.text);//可以读取到
-    //警报 说白了就是弹框
-//    UIAlertView *alert=[[UIAlertView alloc]initWithTitle:@"报警" message:@"读到了" delegate:self cancelButtonTitle:@"取消吧" otherButtonTitles:@"heihei",@"haha", nil];
-//    [alert show];
+    if (sender.tag==15) {
+        UISwitch *swch=(UISwitch *)[self.window viewWithTag:30];//返回的是uiview所以强制转换
+        if (sender.on==YES) {
+            NSLog(@"开关1打开了!");
+            swch.on=YES;
+        }
+        else{
+            NSLog(@"开关1关闭了!");
+            swch.on=NO;
+        }
+    }
+    else if(sender.tag==30)
+    {
+        UISwitch *swch=(UISwitch *)[self.window viewWithTag:15];
+        if (sender.on==YES) {
+            NSLog(@"开关2打开了!");
+            swch.on=YES;
+        }
+        else{
+            NSLog(@"开关2关闭了!");
+            swch.on=NO;
+        }
+    }
+    
+}
+//实现分段控件切换的行为
+-(void)segTap:(UISegmentedControl *)sender
+{
+    label_user.text=[sender titleForSegmentAtIndex:sender.selectedSegmentIndex];
+}
+//实现滑块滑动的行为
+-(void)sliderTap:(UISlider *)sender
+{
+    textfield_user.text=[NSString stringWithFormat:@"%g",sender.value];
 }
 
 - (void)applicationWillResignActive:(UIApplication *)application {
-    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-    // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
 }
 
 - (void)applicationDidEnterBackground:(UIApplication *)application {
-    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
 }
 
 - (void)applicationWillEnterForeground:(UIApplication *)application {
-    // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
 }
 
 - (void)applicationDidBecomeActive:(UIApplication *)application {
-    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
 }
 
 - (void)applicationWillTerminate:(UIApplication *)application {
-    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
 }
 
 @end
